9bce171ebf4526812890aee86352ed65f2900341
Xeeneetee <154092341+Xeeneetee@users.noreply.github.com>
2026-04-27 19:15:29 +0100

    progress on main game code

--- main.py
+++ main.py
@@ -2,7 +2,32 @@
 print("You have arrived in the Frozen Temple.")
 print("After hearing of many civilians never returning after visiting, you've decided to investigate.")
 print("As the mayor of the nearby town, you have a duty to.")
+print("With 2 adventurers in tow, you set off!")
 
 ### First choice
 print("After passing through the gate into the courtyard...")
 print("...there are 3 paths. Ahead, left and right.")
+
+
+### Prompting player with a choice
+roomChoice = input("> ")
+
+if (roomChoice == "Ahead"):
+    print()
+    print()
+    print()
+
+    
+
+elif(roomChoice == "Left"):
+    print()
+    print()
+    print()
+
+elif(roomChoice == "Right"):
+    print()
+    print()
+    print()
+
+else:
+    print("Invalid choice. Please enter Ahead, Left or Right")
---
+++ main_AI.py
@@ -0,0 +1,8 @@
+### Printing a welcome message
+print("You have arrived in the Frozen Temple.")
+print("After hearing of many civilians never returning after visiting, you've decided to investigate.")
+print("As the mayor of the nearby town, you have a duty to.")
+
+### First choice
+print("After passing through the gate into the courtyard...")
+print("...there are 3 paths. Ahead, left and right.")
