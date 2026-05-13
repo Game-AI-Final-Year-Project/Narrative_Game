f9b851460ca3a0b88a29490f3aa38c1ef8603566
Xeeneetee <154092341+Xeeneetee@users.noreply.github.com>
2026-05-13 08:24:55 +0100

    minor changes

--- main_game.py
+++ main_game.py
@@ -1,12 +1,24 @@
+import time
+import sys
+
+def slow_print(text):
+
+    for char in text:
+        sys.stdout.write(char)
+        sys.stdout.flush()
+        time.sleep(0.02)
+
+    print()
+
 def startscreen():
     ### Title screen
 
     while True: 
-        print("Frozen Palace: Non-AI Version")
-        print("START?")
-        print("(Press (1) for Start!)")
-        print("(Press (2) for Help!)")
-        print("(Press (3) for Quit!)")
+        slow_print("Frozen Palace: Non-AI Version")
+        slow_print("START?")
+        slow_print("(Press (1) for Start!)")
+        slow_print("(Press (2) for Help!)")
+        slow_print("(Press (3) for Quit!)")
         startChoice = input("> ")
     
         if (startChoice == "1"):
@@ -14,16 +26,16 @@ def startscreen():
         elif (startChoice == "2"):
             show_help()
         elif (startChoice == "3"):
-            print("Goodbye! Press Ctrl+C to exit!")
+            slow_print("Goodbye! Press Ctrl+C to exit!")
         else:
-            print("Invalid Choice")
+            slow_print("Invalid Choice")
 
 def show_help():
 
-    print("\n=== HELP ===")
-    print("Type the number of your choice.")
-    print("Explore the Frozen Palace.")
-    print("Survive and uncover the mystery of the missing persons.\n")
+    slow_print("\n=== HELP ===")
+    slow_print("Type the number of your choice.")
+    slow_print("Explore the Frozen Palace.")
+    slow_print("Survive and uncover the mystery of the missing persons.\n")
 
     input("Press Enter to return...")
         
@@ -31,79 +43,79 @@ def show_help():
 def adventure_game():
 
     ### Printing a welcome message
-    print("You have arrived in the Frozen Palace.")
-    print("After hearing of many civilians never returning after visiting, you've decided to investigate.")
-    print("As the mayor of the nearby town, you have a duty to.")
-    print("With 2 adventurers in tow, you set off!")
+    slow_print("You have arrived in the Frozen Palace.")
+    slow_print("After hearing of many civilians never returning after visiting, you've decided to investigate.")
+    slow_print("As the mayor of the nearby town, you have a duty to.")
+    slow_print("With 2 adventurers in tow, you set off!")
 
     ### First choice
-    print("After passing through the gate into the courtyard...")
-    print("...there are 3 paths. Ahead, left and right.")
-
+    slow_print("After passing through the gate into the courtyard...")
+    slow_print("...there are 3 paths. Ahead, left and right.")
+    slow_print("(Type [Ahead], [Left], or [Right] to choose)")
 
     ### Prompting player with a choice
     roomChoice = input("> ")
 
-    if (roomChoice == "Ahead"):
+    if (roomChoice == "Ahead" or "ahead"):
     
         hasLight = False
     
-        print("Going in through the main door, you find yourself in the main hall.")
-        print("The hall in front of you and the foyer are empty.")
-        print("There are no signs of life. You notice a single candle and lighter on the console table.")
-        print("Take them?")
-        print("(1. Yes)")
-        print("(2. No)")
+        slow_print("Going in through the main door, you find yourself in the main hall.")
+        slow_print("The hall in front of you and the foyer are empty.")
+        slow_print("There are no signs of life. You notice a single candle and lighter on the console table.")
+        slow_print("Take them?")
+        slow_print("(1. Yes)")
+        slow_print("(2. No)")
 
         ### Choice for player (ahead path)
         choice = input("> ")
         if (choice == "1"):
             hasLight = True
-            print("You grabbed the candle and lighter!")
+            slow_print("You grabbed the candle and lighter!")
 
         elif(choice == "2"):
             hasLight = False
-            print("You left them behind as you travelled on.")
+            slow_print("You left them behind as you travelled on.")
 
         else:
-            print("Invalid choice. Please pick Yes or No")
+            slow_print("Invalid choice. Please pick Yes or No")
     
 
         if hasLight:
-            print("You light your way through the darkness.")
-            print("As you go through, you hear ")
+            slow_print("You light your way through the darkness.")
+            slow_print("As you go through, you hear ")
         else:
-            print("You stumble around, searching for a wall to follow.")
+            slow_print("You stumble around, searching for a wall to follow.")
 
-        print("")
+        slow_print("")
     
 
     elif(roomChoice == "Left"):
-        print("On the left side of the courtyard is a side door leading to an observatory.")
-        print("Though there are windows, they're pitch black.")
-        print("Enter?")
-        print("(1. Yes)")
-        print("(2. No)")
+        slow_print("On the left side of the courtyard is a side door leading to an observatory.")
+        slow_print("Though there are windows, they're pitch black.")
+        slow_print("Enter?")
+        slow_print("(1. Yes)")
+        slow_print("(2. No)")
 
         ### First choice for player on Left route
         choice = input("> ")
         if (choice == "1"):
-            print("")
+            slow_print("")
     
         elif(choice == "2"):
-            print("")
+            slow_print("")
     
         else:
-            print("Invalid choice. Please pick Yes or No")
+            slow_print("Invalid choice. Please pick Yes or No")
 
 
     elif(roomChoice == "Right"):
-        print("")
-        print("")
-        print("")
+        slow_print("")
+        slow_print("")
+        slow_print("")
 
     else:
-        print("Invalid choice. Please enter Ahead, Left or Right")
+        slow_print("Invalid choice. Please enter Ahead, Left or Right")
 
 startscreen()
 adventure_game()
